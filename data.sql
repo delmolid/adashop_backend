0ee77baa9097e72e83841ab8fae7c340e00502cf
Molid <delmolid@hotmail.fr>
2025-05-09 17:21:19 +0200

    integration de la base de données

--- data.sql
+++ data.sql
@@ -5,7 +5,7 @@ CREATE TABLE `User`(
     `password` VARCHAR(255) NOT NULL,
     `role` ENUM('') NOT NULL,
     `adress_id` INT NOT NULL,
-    `seller_id` INT NULL
+    `seller_id` INT UNSIGNED NULL
 );
 CREATE TABLE `Product`(
     `product_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -33,11 +33,11 @@ CREATE TABLE `Order`(
     `date_order` DATETIME NOT NULL,
     `status` ENUM('') NOT NULL,
     `total_amount` DECIMAL(8, 2) NOT NULL,
-    `user_id` INT NOT NULL
+    `user_id` INT UNSIGNED NOT NULL
 );
 CREATE TABLE `Review`(
     `review_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `user_id` INT NOT NULL,
+    `user_id` INT UNSIGNED NOT NULL,
     `rating` ENUM('') NOT NULL,
     `comment` VARCHAR(255) NOT NULL,
     `date_publish` DATETIME NOT NULL
@@ -45,12 +45,12 @@ CREATE TABLE `Review`(
 CREATE TABLE `ShoppingCart`(
     `shopping_cart_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `created_at` DATETIME NOT NULL,
-    `user_id` INT NOT NULL
+    `user_id` INT UNSIGNED NOT NULL
 );
 CREATE TABLE `CartItem`(
     `cart_item_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `shopping_cart_id` INT NOT NULL,
-    `product_id` INT NOT NULL
+    `shopping_cart_id` INT UNSIGNED NOT NULL,
+    `product_id` INT UNSIGNED NOT NULL
 );
 CREATE TABLE `Adress`(
     `adresse_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -58,14 +58,14 @@ CREATE TABLE `Adress`(
     `postal_code` VARCHAR(255) NOT NULL,
     `city` VARCHAR(255) NOT NULL,
     `country` VARCHAR(255) NOT NULL,
-    `user_id` INT NOT NULL
+    `user_id` INT UNSIGNED NOT NULL
 );
 CREATE TABLE `Shipment`(
     `shipment_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `date` DATETIME NOT NULL,
     `type_shipment` ENUM('') NOT NULL,
     `status` ENUM('') NOT NULL,
-    `order_id` INT NOT NULL
+    `order_id` INT UNSIGNED NOT NULL
 );
 CREATE TABLE `Payment`(
     `payment_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -73,17 +73,17 @@ CREATE TABLE `Payment`(
     `payment_method` ENUM('') NOT NULL,
     `status` ENUM('') NOT NULL,
     `payment_date` DATETIME NOT NULL,
-    `order_id` INT NOT NULL
+    `order_id` INT UNSIGNED NOT NULL
 );
 CREATE TABLE `Product_Category`(
     `product_category_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `product_id` INT NOT NULL,
-    `category_id` INT NOT NULL
+    `product_id` INT UNSIGNED NOT NULL,
+    `category_id` INT UNSIGNED NOT NULL
 );
 CREATE TABLE `OrderItem`(
     `order_item_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `product_id` INT NOT NULL,
-    `order_id` INT NOT NULL,
+    `product_id` INT  UNSIGNED NOT NULL,
+    `order_id` INT UNSIGNED NOT NULL,
     `quantity` DOUBLE NOT NULL,
     `price_unit` DOUBLE NOT NULL
 );
